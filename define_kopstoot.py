35efdc77a6052f00ea6ac1308189d3cc180b52f9
JMWitteveen <junior.m.witteveen@gmail.com>
2023-11-21 00:00:15 +0100

    Rewrote define_kopstoot.py
    Rewrote define_kopstoot.py to run on a schedule instead of continuously. It now checks the top 10 hot posts and checks all the comments for mentions of !kopstootkompanen to reply to. While also saving the comments it already replied to, to prevent duplicates.

--- define_kopstoot.py
+++ define_kopstoot.py
@@ -1,19 +1,42 @@
 #!/usr/bin/python
 import praw
 import re
-import random
+import os
 
-
+# Create the Reddit instance
 reddit = praw.Reddit('bot1')
-
-subreddit = reddit.subreddit("pythonforengineers")
-
 kopstoot_definition = "De Kopstoot Kompanen is een groep van ruim 1000 leden exclusief voor lange mensen. Mannen vanaf 1,95m en vrouwen vanaf 1,80m"
 
-for comment in subreddit.stream.comments():
-    print(comment.body)
-    if re.search("!KopstootKompanen", comment.body, re.IGNORECASE):
-            
-            comment.reply(kopstoot_definition)
-            print("replied!")
-            
+# Have we run this code before? If not, create an empty list
+if not os.path.isfile("comments_replied_to.txt"):
+    comments_replied_to = []
+
+# If we have run the code before, load the list of comments we have replied to
+else:
+    # Read the file into a list and remove any empty values
+    with open("comments_replied_to.txt", "r") as f:
+        comments_replied_to = f.read()
+        comments_replied_to = comments_replied_to.split("\n")
+        comments_replied_to = list(filter(None, comments_replied_to))
+
+# Get the top 10 values from our subreddit
+subreddit = reddit.subreddit('pythonforengineers')
+for submission in subreddit.hot(limit=10):
+    # Iterate through comments in the submission
+    for comment in submission.comments.list():
+        # If we haven't replied to this comment before
+        if comment.id not in comments_replied_to:
+            # Do a case insensitive search
+            if re.search("!KopstootKompanen", comment.body, re.IGNORECASE):
+                # Reply to the comment
+                print("Comment content: ", comment.body)
+                comment.reply(kopstoot_definition)
+                print("Bot replying to comment in submission: ", submission.title)
+
+                # Store the current id into our list
+                comments_replied_to.append(comment.id)
+
+# Write our updated list back to the file
+with open("comments_replied_to.txt", "w") as f:
+    for comment_id in comments_replied_to:
+        f.write(comment_id + "\n")
